c8998a788cd3a921a6d18498a7be81d7ba63d7cf
jasanz1994 <josealejandrosanzcuesta@gmail.com>
2025-01-14 12:49:51 -0300

    Deploy Api to FL0

---
+++ Schemas/movie.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const z  = require('zod')
+
+
+const schemaMovie = z.object({
+    title: z.string({
+        invalid_type_error: 'Title must be a string',
+        required_error: 'Title is required'
+    }),
+    year: z.number().int().min(1990).max(2025),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    poster: z.string().url(),
+    rate: z.number().min(0).max(10).default(6),
+    genre: z.array(
+        z.enum(['Action', 'Drama', 'Crime', 'Adventure', 'Sci-Fi',
+            'Romance', 'Animation', 'Biography', 'Fantasy'
+        ]))
+})
+
+function  ValidatedMovie(object){
+    return schemaMovie.safeParse(object)
+}
+
+function ValidatedPartialMovie(object){
+    return schemaMovie.partial().safeParse(object)
+}
+
+module.exports = {
+    ValidatedMovie,
+    ValidatedPartialMovie
+}
+
